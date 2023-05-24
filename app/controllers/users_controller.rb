5c5929300b4175d37e595255095b35fc14c7add3
Luis Alcantara <luisalcan.c@gmail.com>
2023-05-24 17:08:10 -0600

    Signin and signup finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-        @user.send_confirmation_email
-        redirect_to root_path, notice: "Please check your email for confirmation instructions"
+            @user.send_confirmation_email
+            redirect_to root_path, notice: "Please check your email for confirmation instructions"
         else
-        render :new, status: :unprocessable_entity
+            render :new, status: :unprocessable_entity
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
 
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
+
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+  resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
 
   resources :pokemons
 
